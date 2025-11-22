cabeaee7a20506171af04b4591f8c32262cb31cd
myztinen <myztinen@gmail.com>
2025-11-22 16:29:30 +0200

    9.1 - 9.3

---
+++ osa9/bmi/bmiCalculator.ts
@@ -0,0 +1,27 @@
+const arseBarguments = (args: string[]): { height: number; weight: number } => {
+  if (args.length != 4) throw new Error('wrong amount of arguments');
+
+  
+  if (!isNaN(Number(args[2])) || !isNaN(Number(args[3])) || Number(args[2])<= 0 || Number(args[3])<= 0) {
+    return { height: Number(args[2]), weight: Number(args[3])}
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+const calculateBmi = (height: number, weight: number,) : string => {  
+    let result = weight / (height*0.01)**2;
+    if(result < 16.0) return 'Underweight (Severe thinness)';
+        else if (16.0 < result && result < 17.0) return 'Underweight (Moderate thinness)';
+        else if (17.0 < result && result < 18.5) return 'Underweight (Mild thinness)';
+        else if (18.5 < result && result < 25.0) return 'Normal range';
+        else if (25.0 < result && result < 30.0) return 'Overweight (Pre-obese)';
+        else if (30.0 < result && result < 35.0) return 'Obese (Class I)';
+        else if (35.0 < result && result < 40.0) return 'Obese (Class II) ';
+        else if (40.0 < result) return 'Obese (Class III) ';
+        else throw new Error('Promblem with calculating BMI');  
+}
+const {height, weight } = arseBarguments(process.argv)
+console.log(calculateBmi(height, weight))
+
+
---
+++ osa9/bmi/exerciseCalculator.ts
@@ -0,0 +1,62 @@
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  target: number;
+  average: number;
+  success:boolean;
+  rating: number;
+  ratingDescription: string
+}
+
+const parseArguments = (args: string[]): { target: number; exerArray: number[] } => {
+  if (args.length < 4) throw new Error('wrong amount of arguments');
+
+  if (args.slice(2).every(arg => !isNaN(Number(arg)) && Number(arg) >= 0)) {
+    return { target: Number(args[2]), exerArray: args.slice(3).map(arg => Number(arg))}
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+const calculateExercises = (target:number, results:number[]): Result => {
+  const amountOfDays = results.length;
+  const trainingDays = results.filter(day => day > 0).length;
+  const average = Number(results.reduce(((a:number,b:number) => a+b), 0))/ amountOfDays;
+  const isSuccess = average>=target;
+  let theRating:number; 
+  if (average > target-(0.1*target) && (average < target+(0.1*target))) {
+    theRating = 2;
+  } else if (average > target+(0.1*target)) {
+    theRating = 3;
+  } else theRating = 1;
+  let theRatingRant:string; 
+  if (theRating == 2) {
+    theRatingRant = 'not too bad but could be better';
+  } else if (theRating == 3) {
+    theRatingRant = 'Great joB!';
+  } else theRatingRant = 'You suck!';
+  
+  return { 
+    periodLength: amountOfDays,
+    trainingDays: trainingDays,
+    success: isSuccess,
+    rating: theRating,
+    ratingDescription: theRatingRant,
+    target: target,
+    average: average
+    };
+}
+
+try {
+  const {target, exerArray} = parseArguments(process.argv);
+  console.log(calculateExercises(target, exerArray));
+//console.log(calculateExercises(2, [3, 0, 2, 4.5, 0, 3, 1]));
+
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
+
